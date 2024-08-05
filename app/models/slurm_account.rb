a5adeba79d44945e5a8f0df3e3df5e48aec2400a
Pietro Donatini <pietro.donatini@unibo.it>
2024-08-05 12:03:57 +0200

    syncronizeInLdap

--- app/models/slurm_account.rb
+++ app/models/slurm_account.rb
@@ -2,7 +2,22 @@ class SlurmAccount < ApplicationRecord
   has_many :slurm_associations
   has_rich_text :description
 
+  validates :slug, format: {with: /\A[a-z]+[a-z0-9_.]+\z/, message: "Only letters,numbers,underscore,_,dot"}
+
+  after_save :syncronizeInLdap
+
   def to_s
     name
+  end
+
+  def syncronizeInLdap
+    ad2gnu = AD2Gnu::Base.new(:personale, Rails.logger).AD_login.local_login
+    g = AD2Gnu::LocalGroup.new(slug, name)
+
+    if ad2gnu.local.get_group(slug) # if exists
+      ad2gnu.local.update_group(g)
+    else # if to create
+      ad2gnu.local.add_group(g)
+    end
   end
 end
